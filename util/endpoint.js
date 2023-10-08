4392bc77babc5b80561121833df591bfc30f8854
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-10-08 20:37:14 +0530

    update

--- util/endpoint.js
+++ util/endpoint.js
@@ -1,1 +1,1 @@
-export const endpoint = process.env.NODE_ENV === 'production' ? `https://${process.env.VERCEL_URL}/api` :'http://localhost:3000/api';
+export const endpoint = `https://${process.env.VERCEL_URL}/api`
