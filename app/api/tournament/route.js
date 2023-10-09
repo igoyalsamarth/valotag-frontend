1bf47ac4248193400f593d07030dbf35f9e5e683
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-10-09 20:02:23 +0530

    PrismaORM

--- app/api/tournament/route.js
+++ app/api/tournament/route.js
@@ -1,6 +1,31 @@
-import tournaments from '@/data/tournaments.json'
+//import tournaments from '@/data/tournaments.json'
+import prisma from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET(request) {
-  return new NextResponse(JSON.stringify(tournaments.data))
+  let tournaments;
+  try{
+  tournaments = await prisma.tournaments.findMany();
+  } catch (error) {
+    return new NextResponse(error.message, {status:500});
+  }
+  return NextResponse.json(tournaments);
+}
+
+export async function POST(request) {
+  try {
+    const json = await request.json();
+    const tournament = await prisma.tournaments.create({
+      data: json,
+    });
+    return new NextResponse(JSON.stringify(tournament), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    if (error.code === "P2002") {
+      return new NextResponse("Something went rong", { status: 409 });
+    }
+    return new NextResponse(error.message, { status: 500 });
+  }
 }
